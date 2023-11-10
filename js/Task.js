a18d3dfef9d69bbe30b72fbe0768cd6fc23b4cff
brysah <brysahvidal@gmail.com>
2023-11-10 11:07:36 -0300

    feat: add task on button click

--- js/Task.js
+++ js/Task.js
@@ -1,11 +1,32 @@
-export class Task{
-    constructor(root){
-        this.root = document.querySelector(root); 
+export class Task {
+    constructor(root) {
+        this.root = document.querySelector(root);
+        this.entries = [];
     }
+
+    add(value) {
+        const taskExists = this.entries.find(  task => task === value );
+        if(taskExists){
+            console.log('Task already exist');
+            return;
+        }  
+        this.entries = [value,...this.entries];
+        this.update();
+    }
+
 }
 
-export class TaskView extends Task{
-    constructor(root){
+export class TaskView extends Task {
+    constructor(root) {
         super(root);
+        this.onadd();
+    }
+    onadd() {
+        const addButton = document.querySelector('.create');
+        addButton.onclick = () => {
+            const { value } = document.getElementById('input-task');
+            console.log(value);
+            this.add(value);
+        }
     }
 }
